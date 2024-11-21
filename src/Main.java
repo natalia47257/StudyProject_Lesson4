ca3e8a84d7617c1cc2bb6f95d59851fc0ebb5ca2
natalia47256 <nataliap1111@yahoo.com>
2024-11-21 11:46:02 +0100

    hello

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+import lesson4.Lesson4;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    String str = "I study Java";
+
+    System.out.println(str.length());
+
+    System.out.println(str.charAt(4));
+
+    System.out.println(str.substring(3,str.length()));
+    }
+}
